91bf42c731d9edaa911e78a874d55ec8578afd01
unknown <THYAI99@WP0816.ATOMIUM.local>
2014-12-15 21:09:43 +0100

    Initial setup

---
+++ Entities/BClass.cs
@@ -0,0 +1,25 @@
+﻿using BuilderFactory.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderFactory.Entities
+{
+    public class BClass
+    {
+        public BClass()
+        {
+            Usings = new List<BUsing>();
+            Constructors = new List<BConstructor>();
+            Properties = new List<BProperty>();
+        }
+
+        public string Namespace { get; set; }
+        public IList<BUsing> Usings { get; set; }
+        public IList<BConstructor>  Constructors { get; set; }
+        public IList<BProperty> Properties{ get; set; }
+
+    }
+}
---
+++ Entities/BClassBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderFactory.Entities
+{
+    public class BClassBuilder
+    {
+        private BClass _BClass;
+
+        public BClass Create()
+        {
+            return _BClass;
+        }
+
+        public static BClassBuilder Init()
+        {
+            return new BClassBuilder();
+        }
+
+        public static implicit operator BClass(BClassBuilder builder)
+        {
+            return builder.Create();
+        }
+
+        public BClassBuilder Namespace(string value)
+        {
+            _BClass.Namespace = value;
+            return this;
+        }
+
+        public BClassBuilder AddUsing(BUsing value)
+        {
+            _BClass.Usings.Add(value);           
+            return this;
+        }
+
+        public BClassBuilder AddUsings(Collection<BUsing> values)
+        {
+            foreach (BUsing bUsing in values)
+            {
+                AddUsing(bUsing);
+            }
+            return this;
+        }
+
+        public BClassBuilder AddConstructor(BConstructor value)
+        {
+            _BClass.Constructors.Add(value);            
+            return this;
+        }
+
+        public BClassBuilder AddConstructors(Collection<BConstructor> values)
+        {
+            foreach (BConstructor bConstructor in values)
+            {
+                AddConstructor(bConstructor);
+            }
+            return this;
+        }
+
+        public BClassBuilder AddProperty(BProperty value)
+        {           
+            _BClass.Properties.Add(value);            
+            return this;
+        }
+
+        public BClassBuilder AddProperties(Collection<BProperty> values)
+        {
+            foreach (BProperty bProperty in values)
+            {
+                AddProperty(bProperty);
+            }
+            return this;
+        }
+
+    }
+    
+}
